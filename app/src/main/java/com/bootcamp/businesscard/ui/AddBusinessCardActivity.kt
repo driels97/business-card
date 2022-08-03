7515fcd448e7def9d1455dcf2a9cf7de47b1e0b5
Adriel Venancio Bomfim <adriel@polkadex.trade>
2022-08-03 11:38:09 -0300

    feat: configuração do banco de dados interno e integração de dados com ui

--- app/src/main/java/com/bootcamp/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/bootcamp/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,10 +2,18 @@ package com.bootcamp.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.bootcamp.businesscard.App
+import com.bootcamp.businesscard.R
+import com.bootcamp.businesscard.data.BusinessCard
 import com.bootcamp.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +26,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
 
-        binding.btnConfirma.setOnClickListener {
+        binding.btnConfirm.setOnClickListener {
+            val businessCard = BusinessCard(
+                nome = binding.tilNome.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilEmpresa.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString(),
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.show_success, Toast.LENGTH_SHORT).show()
             finish()
         }
     }
--- app/src/main/java/com/bootcamp/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/bootcamp/businesscard/ui/MainActivity.kt
@@ -3,14 +3,22 @@ package com.bootcamp.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.bootcamp.businesscard.App
 import com.bootcamp.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -18,6 +26,12 @@ class MainActivity : AppCompatActivity() {
         binding.fabAdd.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) {businessCards ->
+            adapter.submitList(businessCards)
         }
     }
 }
